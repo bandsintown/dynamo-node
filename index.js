f6afe30bab5d4dd44a67746c3b9c81cc78ebbd2e
marcogadget <mgordon@bandsintown.com>
2017-10-02 17:40:27 -0400

    Fix conflict with Global AWS.config

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ module.exports = (region = 'eu-central-1', config) => {
   var dynamoDB, configObj;
 
   if (typeof config === 'string') {
-    configObj = Object.assign(JSON.parse(require('fs').readFileSync(config, 'utf8')), { region: region } );
+    configObj = Object.assign(JSON.parse(require('fs').readFileSync(config, 'utf8')) );
   } else if (typeof config === 'object') {
-    configObj = Object.assign(config, { region: region })
-  } else {
-    // This will force using STS as fallback credentials provider
-    if (!configObj) configObj = new AWS.ECSCredentials();
-    if (!configObj) configObj = new AWS.SharedIniFileCredentials();
-    if (!configObj) configObj = new AWS.EnvironmentCredentials('AWS');
+    configObj = config
+  }
+
+  // This will force using STS as fallback credentials provider
+  if (!Object.keys(configObj).length) configObj = new AWS.ECSCredentials();
+  // if (!Object.keys(configObj).length) configObj = new AWS.SharedIniFileCredentials();
+  // if (!Object.keys(configObj).length) configObj = new AWS.EnvironmentCredentials('AWS');
+
+  // Merge the region into config object
+  if (region) {
+    configObj = Object.assign(configObj, { region: region })
   }
 
   dynamoDB = new AWS.DynamoDB( configObj );
